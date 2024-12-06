965f6fad7806620ba63d09e80b4c0acdb576df7e
Stanisław Flak <stazus@interia.pl>
2024-12-06 04:46:23 +0100

    Kliknięty link otrzymuje klasę active

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log('Event object:', event); // Dodano logowanie argumentu event
+  const clickedElement = this;
+  console.log('Link was clicked!'); 
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active'); // Dodanie klasy 'active' do klikniętego elementu
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active'); // Zmieniony selektor na odpowiedni dla artykułów
 
   for (let activeArticle of activeArticles) {
