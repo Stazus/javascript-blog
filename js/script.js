3023ceb9e85b55e2894a62c041f1d6afcb98e1db
Stanisław Flak <stazus@interia.pl>
2024-12-15 14:30:47 +0100

    Akcja po kliknięciu w tag - dodanie szablonu

--- js/script.js
+++ js/script.js
@@ -119,3 +119,53 @@ function generateTags() {
 
 // Wywołanie funkcji
 generateTags();
+
+// Funkcja obsługująca kliknięcia w tagi
+function tagClickHandler(event){
+  event.preventDefault(); // Zapobiegnięcie domyślnemu działaniu linków
+
+  const clickedElement = this; // Kliknięty element
+
+  const href = clickedElement.getAttribute('href'); // Pobranie atrybutu 'href' klikniętego elementu
+  const tag = href.replace('#tag-', ''); // Wyciągnięcie tagu z atrybutu 'href'
+  console.log('Selected tag:', tag);
+
+  /* find all tag links with class active */
+  const activeLinks = document.querySelectorAll('.post-tags a.active');
+
+  /* START LOOP: for each active tag link */
+  for (let activeLink of activeLinks) {
+    /* remove class active */
+    activeLink.classList.remove('active');
+  }
+  /* END LOOP: for each active tag link */
+
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  const selectedTagLinks = document.querySelectorAll(`a[href="${href}"]`);
+
+  /* START LOOP: for each found tag link */
+  for (let selectedTagLink of selectedTagLinks) {
+    /* add class active */
+    selectedTagLink.classList.add('active');
+  }
+  /* END LOOP: for each found tag link */
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks();
+}
+
+// Funkcja dodająca nasłuchiwacze do kliknięć w tagi
+function addClickListenersToTags(){
+  /* find all links to tags */
+  const tagLinks = document.querySelectorAll('.post-tags a');
+
+  /* START LOOP: for each link */
+  for (let tagLink of tagLinks) {
+    /* add tagClickHandler as event listener for that link */
+    tagLink.addEventListener('click', tagClickHandler);
+  }
+  /* END LOOP: for each link */
+}
+
+// Wywołanie funkcji, aby dodać nasłuchiwacze do tagów
+addClickListenersToTags();
